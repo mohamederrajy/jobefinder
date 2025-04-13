cb00755c8526789aafdb2bac370ba01fb5db6b51
AZIZ <aziz@AZIZs-MacBook-Pro.local>
2025-04-13 05:15:44 +0100

    buil

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,11 +12,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,  // Set to 'index.html' if you want client-side routing for 404 pages
-		  }),
+			out: 'build'
+		})
 	}
 };
 
